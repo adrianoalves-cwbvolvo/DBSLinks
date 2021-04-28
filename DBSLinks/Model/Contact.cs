ee0edee52ed0ffc34f592289035949e6fd9e56cf
Silva Adriano (6) <adriano.silva.6@volvo.com>
2021-04-28 15:44:42 -0300

    Update Contact.cs
    Making Public

--- DBSLinks/Model/Contact.cs
+++ DBSLinks/Model/Contact.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LinksForm.Model
 {
-    class Contact
+    public class Contact
     {
         public string Id { get; set; }
         public string Name { get; set; }
