44ad7246e97461d2d57531c63a10cf8904e5d78d
Silva Adriano (6) <adriano.silva.6@volvo.com>
2021-04-29 07:54:52 -0300

    Update frmAddOrUpdateDealer.cs

--- DBSLinks/Forms/frmAddOrUpdateDealer.cs
+++ DBSLinks/Forms/frmAddOrUpdateDealer.cs
@@ -15,139 +15,93 @@ namespace LinksForm.Forms
 {
     public partial class frmAddOrUpdateDealer : Form
     {
+        List<Dealer> dealerList = new List<Dealer>();
         List<Dealer> mainDealerList = new List<Dealer>();
         List<Country> countryList = new List<Country>();
-        private int GlobalDealerId;
 
-        public frmAddOrUpdateDealer(int DealerId, string DealerCode, string DealerName, int CountryId)
+        int CountryId;
+
+        public bool HasTheCancelButtonPressed { get; set; }
+
+        public frmAddOrUpdateDealer(Dealer dealer)
         {
             InitializeComponent();
 
-            GlobalDealerId = DealerId;
+            CountryId = dealer.CountryId;
 
-            #region "DEALER - UPDATE"
+            dealerList = DALHelpers.GetDealers();
+            countryList = DALHelpers.GetCountries();
 
-            if (!string.IsNullOrEmpty(DealerName))
+            HasTheCancelButtonPressed = false;
+
+            int counter = 0;
+
+            if (dealer.DealerId != 0)
             {
-                int counter = 0;
-
-                this.Text = "Update Dealer";
-                txtDealerCode.Text = DealerCode;
-                txtDealerName.Text = DealerName;
-
-                countryList = DALHelpers.GetCountries();
-
-                Dictionary<int, Dealer> comboDealer = new Dictionary<int, Dealer>();
-                Dictionary<int, Country> comboBox = new Dictionary<int, Country>();
-
                 foreach (Country country in countryList)
                 {
                     cmbCountries.Items.Add(country.CountryName.ToString());
-                    comboBox.Add(counter, country);
 
-                    if (CountryId == country.CountryId)
+                    if (dealer.CountryId == country.CountryId)
                     {
                         cmbCountries.SelectedIndex = counter;
                     }
 
                     counter++;
                 }
+
+                this.Text = "Update Dealer Branch";
+                txtBranchName.Text = dealer.DealerName;
+                txtPhoneNumber.Text = dealer.PhoneNumber;
+                txtCTDI.Text = dealer.CTDI;
+                txtBaldoPartner.Text = dealer.BaldoPartner;
             }
-
-            #endregion
-
-            #region "DEALER - NEW"
             else
             {
-                //LOAD DATA FOR NEW SUBDEALER
-                countryList = DALHelpers.GetCountries();
-
                 foreach (Country country in countryList)
                 {
                     cmbCountries.Items.Add(country.CountryName.ToString());
                 }
+
+                this.Text = "New Dealer Brach";
+
             }
 
-            #endregion
+            cmbCountries.Focus();
         }
-
-        #region "SAVE"
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-            Dealer dealer = new Dealer();
-            bool ok = false;
-            string comboCountries;
-
-            comboCountries = cmbCountries.Text;
-
-            if (Validation.DealerDataValidation(txtDealerCode.Text, txtDealerName.Text, comboCountries))
-            {
-                MessageBox.Show("Please enter the Dealer details.");
-                return;
-            }
-            else
-            {
-                dealer.DealerName = txtDealerName.Text;
-                dealer.DealerId = GlobalDealerId;
-
-                foreach (Country country in countryList)
-                {
-                    if (cmbCountries.Text == country.CountryName)
-                    {
-                        dealer.CountryId = country.CountryId;
-                    }
-                }
-            }
-
-            #endregion
-
-            #region "UPDATE"
-
-            //UPDATE
-            if (GlobalDealerId > 0)
-            {
-
-                //ok = DALHelpers.UpdateDealer(dealer);
-
-                if (ok == true)
-                {
-                    MessageBox.Show("The record was successfully saved!");
-                }
-                else
-                {
-                    MessageBox.Show("Error: An error has ocurred when trying to update the Dealer!");
-                }
-
-                this.Close();
-            }
-
-            #endregion
-
-            #region "NEW"
-
-            else
-            {
-                //NEW
-                //ok = DALHelpers.AddDealer(dealer);
-
-                if (ok == true)
-                {
-                    MessageBox.Show("The record was successfully saved!");
-                }
-                else
-                {
-                    MessageBox.Show("Error: An error has ocurred when trying to add a new Dealer!");
-                }
-
-                this.Close();
-            }
-
-            #endregion
-        }
-
+   
         private void btnExit_Click(object sender, EventArgs e)
         {
+            HasTheCancelButtonPressed = true;
             this.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbCountries_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            foreach (Country country in countryList)
+            {
+                if (cmbCountries.SelectedItem.ToString() == country.CountryName)
+                {
+                    cmbMainDealer.Items.Clear();
+                    mainDealerList.Clear();
+                    mainDealerList = DALHelpers.GetMainDealersByCountry(country.CountryId);
+                }
+            }
+
+            foreach (Dealer mainDealer in mainDealerList)
+            {
+                cmbMainDealer.Items.Add(mainDealer.DealerName.ToString());
+            }
+        }
+
+        private void cmbCountries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
