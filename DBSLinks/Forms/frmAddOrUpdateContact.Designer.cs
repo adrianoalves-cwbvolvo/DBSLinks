c0c60f4957752f6cf466522352b5c364036737dd
Silva Adriano (6) <adriano.silva.6@volvo.com>
2021-04-29 07:54:25 -0300

    Update frmAddOrUpdateContact.Designer.cs

--- DBSLinks/Forms/frmAddOrUpdateContact.Designer.cs
+++ DBSLinks/Forms/frmAddOrUpdateContact.Designer.cs
@@ -39,13 +39,17 @@
             this.txtComputer = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCellPhone = new System.Windows.Forms.TextBox();
+            this.lblCellPhone = new System.Windows.Forms.Label();
+            this.lblTeam = new System.Windows.Forms.Label();
+            this.cmbTeam = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(12, 57);
+            this.lblName.Location = new System.Drawing.Point(16, 88);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(38, 13);
             this.lblName.TabIndex = 0;
@@ -53,45 +57,45 @@
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(78, 54);
+            this.txtName.Location = new System.Drawing.Point(82, 85);
             this.txtName.MaxLength = 40;
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(153, 20);
-            this.txtName.TabIndex = 1;
+            this.txtName.Size = new System.Drawing.Size(301, 20);
+            this.txtName.TabIndex = 3;
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(78, 80);
+            this.txtPhone.Location = new System.Drawing.Point(82, 111);
             this.txtPhone.MaxLength = 25;
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(112, 20);
-            this.txtPhone.TabIndex = 3;
+            this.txtPhone.Size = new System.Drawing.Size(207, 20);
+            this.txtPhone.TabIndex = 4;
             // 
             // lblPhone
             // 
             this.lblPhone.AutoSize = true;
-            this.lblPhone.Location = new System.Drawing.Point(12, 83);
+            this.lblPhone.Location = new System.Drawing.Point(16, 114);
             this.lblPhone.Name = "lblPhone";
             this.lblPhone.Size = new System.Drawing.Size(41, 13);
-            this.lblPhone.TabIndex = 2;
+            this.lblPhone.TabIndex = 0;
             this.lblPhone.Text = "Phone:";
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(44, 175);
+            this.btnSave.Location = new System.Drawing.Point(139, 238);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(94, 43);
-            this.btnSave.TabIndex = 4;
+            this.btnSave.TabIndex = 7;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(144, 175);
+            this.btnExit.Location = new System.Drawing.Point(239, 238);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(94, 43);
-            this.btnExit.TabIndex = 5;
+            this.btnExit.TabIndex = 8;
             this.btnExit.Text = "Cancel";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
@@ -99,7 +103,7 @@
             // lblId
             // 
             this.lblId.AutoSize = true;
-            this.lblId.Location = new System.Drawing.Point(12, 31);
+            this.lblId.Location = new System.Drawing.Point(16, 62);
             this.lblId.Name = "lblId";
             this.lblId.Size = new System.Drawing.Size(46, 13);
             this.lblId.TabIndex = 0;
@@ -108,50 +112,89 @@
             // txtUserID
             // 
             this.txtUserID.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txtUserID.Location = new System.Drawing.Point(78, 28);
+            this.txtUserID.Location = new System.Drawing.Point(82, 59);
             this.txtUserID.MaxLength = 9;
             this.txtUserID.Name = "txtUserID";
             this.txtUserID.Size = new System.Drawing.Size(83, 20);
-            this.txtUserID.TabIndex = 1;
+            this.txtUserID.TabIndex = 2;
             // 
             // txtComputer
             // 
-            this.txtComputer.Location = new System.Drawing.Point(78, 106);
+            this.txtComputer.Location = new System.Drawing.Point(82, 163);
             this.txtComputer.MaxLength = 25;
             this.txtComputer.Name = "txtComputer";
-            this.txtComputer.Size = new System.Drawing.Size(112, 20);
-            this.txtComputer.TabIndex = 7;
+            this.txtComputer.Size = new System.Drawing.Size(207, 20);
+            this.txtComputer.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 109);
+            this.label1.Location = new System.Drawing.Point(16, 166);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 6;
+            this.label1.TabIndex = 0;
             this.label1.Text = "Computer:";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.lblTeam);
+            this.groupBox1.Controls.Add(this.cmbTeam);
             this.groupBox1.Controls.Add(this.lblId);
             this.groupBox1.Controls.Add(this.txtComputer);
             this.groupBox1.Controls.Add(this.txtUserID);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.lblName);
             this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.lblCellPhone);
             this.groupBox1.Controls.Add(this.lblPhone);
+            this.groupBox1.Controls.Add(this.txtCellPhone);
             this.groupBox1.Controls.Add(this.txtPhone);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(247, 144);
+            this.groupBox1.Size = new System.Drawing.Size(438, 214);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
+            // 
+            // txtCellPhone
+            // 
+            this.txtCellPhone.Location = new System.Drawing.Point(82, 137);
+            this.txtCellPhone.MaxLength = 25;
+            this.txtCellPhone.Name = "txtCellPhone";
+            this.txtCellPhone.Size = new System.Drawing.Size(207, 20);
+            this.txtCellPhone.TabIndex = 5;
+            // 
+            // lblCellPhone
+            // 
+            this.lblCellPhone.AutoSize = true;
+            this.lblCellPhone.Location = new System.Drawing.Point(16, 140);
+            this.lblCellPhone.Name = "lblCellPhone";
+            this.lblCellPhone.Size = new System.Drawing.Size(61, 13);
+            this.lblCellPhone.TabIndex = 0;
+            this.lblCellPhone.Text = "Cell Phone:";
+            // 
+            // lblTeam
+            // 
+            this.lblTeam.AutoSize = true;
+            this.lblTeam.Location = new System.Drawing.Point(16, 35);
+            this.lblTeam.Name = "lblTeam";
+            this.lblTeam.Size = new System.Drawing.Size(37, 13);
+            this.lblTeam.TabIndex = 0;
+            this.lblTeam.Text = "Team:";
+            // 
+            // cmbTeam
+            // 
+            this.cmbTeam.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTeam.FormattingEnabled = true;
+            this.cmbTeam.Location = new System.Drawing.Point(82, 32);
+            this.cmbTeam.Name = "cmbTeam";
+            this.cmbTeam.Size = new System.Drawing.Size(301, 21);
+            this.cmbTeam.TabIndex = 1;
             // 
             // frmAddOrUpdateContact
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(270, 230);
+            this.ClientSize = new System.Drawing.Size(462, 290);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnSave);
@@ -179,5 +222,9 @@
         private System.Windows.Forms.TextBox txtComputer;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblCellPhone;
+        private System.Windows.Forms.TextBox txtCellPhone;
+        private System.Windows.Forms.Label lblTeam;
+        private System.Windows.Forms.ComboBox cmbTeam;
     }
 }
