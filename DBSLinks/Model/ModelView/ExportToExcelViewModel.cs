b58ec13868513b4a987c9c3c6ac32d1ca1f72015
Silva Adriano (6) <adriano.silva.6@volvo.com>
2021-09-15 12:46:50 -0300

    Create ExportToExcelViewModel.cs

---
+++ DBSLinks/Model/ModelView/ExportToExcelViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Links.Model.ModelView
+{
+    public class ExportToExcelViewModel
+    {
+        public int Col { get; set; }
+        public int Row { get; set; }
+        public string Text { get; set; }
+    }
+}
